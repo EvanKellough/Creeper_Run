2a7941830c1c48f1748e5ad173b60e77d4a6931b
Evan Kellough <evan.kellough@maine.edu>
2015-08-27 23:15:10 -0400

    saving my work real quickzz

--- Vault 6/src/com/zaino/vault6/Game.java
+++ Vault 6/src/com/zaino/vault6/Game.java
@@ -7,6 +7,7 @@
 package com.zaino.vault6;
 
 import java.awt.*;
+import java.util.Scanner;
 import javax.swing.*;
 
 import com.zaino.vault6.*; //imports all game files
@@ -14,45 +15,72 @@ import com.zaino.vault6.*; //imports all game files
 public class Game {
 	
 	public static void main(String[] args){
-		boolean lt, rt = false;
+
+		boolean win = false;
+		int choice = 0;
+		
+		Scanner Scan = new Scanner(System.in);
+		System.out.println("Welcome to Vault 6!  A fan made indie text adventure game based on the cancelled");
+		System.out.println("2000 game Fallout Extreme!  Please Enter 0 to begin your journey!");
+		choice = Scan.nextInt();
+		
+		try{
+		switch (choice){
+			case 0:
+				System.out.println("You begin your journey");
+				choice = Scan.nextInt();
+				break;
+			case 1:
+				System.out.println("a");
+				choice = Scan.nextInt();
+				break;
+			case 2:
+				System.out.println("b");
+				choice = Scan.nextInt();
+				break;
+			case 3:
+				System.out.println("c");
+				choice = Scan.nextInt();
+				break;
+			case 4:
+				System.out.println("d");
+				choice = Scan.nextInt();
+				break;
+			case 5:
+				System.out.println("e");
+				choice = Scan.nextInt();
+				break;
+			case 6:
+				System.out.println("f");
+				break;
+			case 7:
+				System.out.println("g");
+				break;
+			case 8:
+				System.out.println("h");
+				break;
+			case 9:
+				System.out.println("");
+				break;
+			case 10:
+				System.out.println("You have completed your journey!  Congratulations!");
+				win = true;
+				break;
+			}
+		}
+		Finally{
+		//System.out.println("You have completed your journey!  Congratulations!");
+	}
+}
+
+/*		boolean lt, rt = false;
 		JPanel scene = new JPanel ();
 		JButton leftbutton = new JButton ();
 		JButton rightbutton = new JButton ();
-		
-		String scenetext; //describes the situation
-		String choicea; //Your left choice
-		String choiceb; //Your right choice
-		
-		int a = 600; //window width
-		int b = 600; //window height
-		
-		//The below section is just for centering the window
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = screenSize.getWidth(); //get screen width
-		double height = screenSize.getHeight();//get screen height
-		int x = (int) (width); //width of screen converted to int
-		int y = (int) (height); //height of screen converted to int
-		int xb = (x / 2) - (a / 2); //offset by window size
-		int yb = (y / 2) - (b / 2); //offset by window size
-		
+		//GUI Stuff for version 2
 		JFrame frame;
 		frame = new JFrame("Vault 6");
 		frame.setVisible( true );
 		frame.setSize(600, 600);
 		frame.setDefaultCloseOperation(1);
-		frame.setLocation(xb, yb);
-		
-		//bounds
-		scene.setBounds(0, 0, a, b/2);
-		leftbutton.setBounds(0, 300, a/2, b/2);
-		rightbutton.setBounds(0, 300, a/2, b/2);
-		
-		//Eventually I want to add textures to the buttons
-		/*scene.setBackground(bg);
-		leftbutton.setBackground(bg);
-		rightbutton.setBackground(bg);*/
-		
-	}
-	
-	
-}
+		frame.setLocationRelativeTo(null);*/
